5620a5adefde49d7bef53327a44122483fda8452
Guilherme Silva <guilherme@planisa.com.br>
2025-02-20 20:40:29 -0300

    CRUD tag impl + user impl + action text install

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,50 @@
+class TagsController < ApplicationController
+  allow_unauthenticated_access only: %i[ index show ]
+  before_action :set_tag, only: %i[ show edit update destroy ]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to @tag
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @tag.update(tag_params)
+      redirect_to @tag
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tag.destroy
+    redirect_to tags_path
+  end
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.expect(tag: [:name])
+  end
+end
